6872498dc7a66778a05dc8415d24f162fff34567
Michaela Angus <michaela-angus@uiowa.edu>
2026-04-06 12:34:00 -0500

    Add Jinja templates and CSS

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 # Michaela Angus 4/6/2026
 
-from flask import Flask
-app = Flask(__name__)  # Michaela
+from flask import Flask, render_template
+app = Flask(__name__)  
 
 @app.route("/")
 def home_page():
-    return "Hello world!!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5002)
