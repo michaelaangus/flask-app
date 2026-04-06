f64e62aeb1becaa82748f24a49ec79ca4f00c709
Michaela Angus <michaela-angus@uiowa.edu>
2026-04-06 12:51:06 -0500

    Adding about.html

--- app.py
+++ app.py
@@ -3,9 +3,9 @@
 from flask import Flask, render_template
 app = Flask(__name__)  
 
-@app.route("/")
+@app.route("/about")
 def home_page():
-    return render_template('index.html')
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5002)
